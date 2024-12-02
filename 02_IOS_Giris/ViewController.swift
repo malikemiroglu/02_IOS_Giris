abc56266446f365c8c70fcc21502ec4c29285792
Malik <Malik@Mualla-MacBook-Air.local>
2024-12-02 20:17:41 +0300

    Initial commit

---
+++ 02_IOS_Giris/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  02_IOS_Giris
+//
+//  Created by Malik on 2.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
