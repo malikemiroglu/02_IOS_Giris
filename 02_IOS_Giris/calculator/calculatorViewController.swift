7e6191eba6670d9ae4fdc134a3eed441e0b3cc08
MalikEmiroglu <malikemiroglu92@gmail.com>
2024-12-03 15:35:23 +0300

    Calculator

---
+++ 02_IOS_Giris/calculator/calculatorViewController.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorViewController.swift
+//  02_IOS_Giris
+//
+//  Created by Malik on 3.12.2024.
+//
+
+import UIKit
+
+class calculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
